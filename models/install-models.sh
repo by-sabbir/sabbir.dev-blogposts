837f19b302f5524e504fe8c1da0d8afca887efa3
Sabbir Ahmed <su@sabbir.dev>
2020-10-03 04:13:16 +0600

    models downloader

---
+++ models/install-models.sh
@@ -0,0 +1,4 @@
+wget http://dlib.net/files/mmod_human_face_detector.dat.bz2
+wget https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml
+echo "##############################"
+echo "Download Completed"
